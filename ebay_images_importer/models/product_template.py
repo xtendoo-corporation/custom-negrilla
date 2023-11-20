ce6f9957fa71c3e08a26ef1c15dfd852f4ef64f3
Manuel Calero <manuelcalerosolis@gmail.com>
2023-11-20 18:26:03 +0100

    ebay importer image correct duplicate image

--- ebay_images_importer/models/product_template.py
+++ ebay_images_importer/models/product_template.py
@@ -9,14 +9,15 @@ class ProductTemplate(models.Model):
 
     def action_ebay_images_importer(self):
         for record in self:
-            for image in record.ept_image_ids:
-                image_vals = {
-                    "name": image.url,
-                    "image_1920": image.image,
-                    "product_tmpl_id": record.id,
-                }
-                if len(self.env["product.image"].search([("name", "=", image.url)])) == 0:
-                    self.env["product.image"].create(image_vals)
+            if len(record.ept_image_ids) > 1:
+                for image in record.ept_image_ids[1:]:
+                    image_vals = {
+                        "name": image.url,
+                        "image_1920": image.image,
+                        "product_tmpl_id": record.id,
+                    }
+                    if len(self.env["product.image"].search([("name", "=", image.url)])) == 0:
+                        self.env["product.image"].create(image_vals)
 
             if not record.image_1920 and record.ept_image_ids:
                 record.image_1920 = record.ept_image_ids[0].image
