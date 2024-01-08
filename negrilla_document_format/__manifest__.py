4af7a8c740c3b5e3f1ff845f10eba01a422fe501
Dani <dani.xtendoo@gmail.com>
2024-01-08 13:54:44 +0100

    [ADD] negrilla_document_format

---
+++ negrilla_document_format/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Negrilla Document Format",
+    "summary": """Formatos de documentos entregados por Xtendoo""",
+    "version": "15.1.0.0",
+    "description": """Formatos de documentos entregados por Xtendoo""",
+    "author": "Daniel Dominguez",
+    "company": "Xtendoo",
+    "website": "http://xtendoo.es",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+        "base",
+
+    ],
+    "data": [
+        "views/invoice/invoice_document.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
