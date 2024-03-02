c0b8e1770a53ca097b6af76829e7375dd87fd9f5
manuelcalerosolis <manuelcalerosolis@gmail.com>
2024-03-02 12:48:51 +0100

    website sale product

---
+++ negrilla_website_sale_product/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Manuel Calero - Xtendoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Negrilla website sale product",
+    "version": "16.0.2.0.0",
+    "category": "Website",
+    "author": "Manuel Calero Solis - Xtendoo",
+    "license": "AGPL-3",
+    "summary": "Negrilla website sale product",
+    "depends": [
+        "website_sale",
+    ],
+    "data": [
+        "views/website_sale_template.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
+
